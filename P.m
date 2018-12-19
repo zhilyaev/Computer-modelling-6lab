85d2d6afe536d436dfaed5fa5f1921c7f2e76d1c
@Diamon <zhilyaev@users.noreply.github.com>
2018-12-19 03:51:57 +0300

    мой вариант

--- P.m
+++ P.m
@@ -1,14 +1,9 @@
-function [y] = P(i,t)
-N=11;
-l=0.3;
-T=N/2+N*l/4;
-my=1/T;
-lja=1/N;
+function [y] = P(i, t, mu, l)
 y=0;
-if(i==0)
-    y=my/(my+lja)+lja/(my+lja)*exp(-t*(lja+my));
+if(i == 0)
+     y= mu / (mu+l)+l / (mu+l)*exp(-t*(l+mu));
 end
-if(i==1)
-    y=lja/(my+lja)-lja/(my+lja)*exp(-t*(lja+my));
+if(i == 1)
+    y = l / (mu+l)-l / (mu+l)*exp(-t*(l+mu));
 end
 end
